ab618d5a9a129e927fbb94fdcaaff243b76e6a1e
bo <277366155@163.com>
2020-07-08 23:32:00 +0800

    添加测试方法

--- ConsoleTest/RabbitMqTest.cs
+++ ConsoleTest/RabbitMqTest.cs
@@ -9,17 +9,26 @@ namespace MH.ConsoleTest
 {
     public class RabbitMqTest
     {
-        static RabbitMqOptions options = BaseCore.Configuration.GetSection("RabbitMqConfig").Get<RabbitMqOptions>();
+        static RabbitMqOptions _options = BaseCore.Configuration.GetSection("RabbitMqConfig").Get<RabbitMqOptions>();
 		static RabbitMqOptions options2 = BaseCore.Configuration.GetSection("RabbitMqConfig2").Get<RabbitMqOptions>();
-		//public void Consumer()
-  //      {
-  //          var consumer = new Consumer(options);
-  //          consumer.ConsumeStart<dynamic>((a)=> {
-  //              Console.WriteLine("Consumer1 收到数据：" + a);
-  //          });
-  //      }
+        public void Consumer(RabbitMqOptions options)
+        {
+            if (options != null)
+            {
+                _options = options;
+            }
+            Consumer();
+        }
+        public void Consumer()
+        {
+            var consumer = new Consumer(_options);
+            consumer.ConsumeStart<dynamic>((a) =>
+            {
+                Console.WriteLine("Consumer1 收到数据：" + a);
+            });
+        }
 
-		public void Consumer2()
+        public void Consumer2()
 		{
 			var consumer = new Consumer(options2);
 			consumer.ConsumeStart<dynamic>((a) => {
@@ -29,7 +38,7 @@ namespace MH.ConsoleTest
 
 		public void Publisher()
         {
-            var publisher = new Publisher(options);
+            var publisher = new Publisher(_options);
             publisher.PublisherStart(new { age=Console.ReadLine(),name=Console.ReadLine()});
         }
     }
